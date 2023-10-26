dd60959b57819221ef564ead0f341476a1923b7c
Your Name <you@example.com>
2023-10-26 17:14:57 +0300

    add hamburger

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,14 @@
-  
+$(document).ready(function () {
+
+    $('.your-class').slick({
+        dots: true,
+    })
+
+
+
+const burger = document.querySelector('.hamburger');
+
+    burger.addEventListener('click', function () {
+        // burger.classList.replace("test-delete", "bar");
+        burger.classList.toggle('is-active')
+    })
