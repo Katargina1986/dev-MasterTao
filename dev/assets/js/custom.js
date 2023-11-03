bf76afc11f110d2a42baedf74160152a0b4b1403
Your Name <you@example.com>
2023-11-04 00:59:52 +0200

    first-screen-mob

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
-
+   
     $('.first-screen-slider').slick({
         dots: true,
         appendArrows: '.first-screen-nav',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                arrows: false,
+              }
+            },
+          ]
     });
-
 
 const burger = document.querySelector('.hamburger');
 
