4e2377d65b0ab9299e85777bd7d89d40d4f1f8ed
Your Name <you@example.com>
2023-11-06 23:33:29 +0200

    add advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -31,3 +31,29 @@ checkButton.forEach((item, index) => {
   item.closest('.calculation-column').style.marginBottom = "-10px";
   index === 0 ? item.closest('.input-box').classList.add('has-checkbox') : null;
 })
+
+ mobileOnlySlider("#advantages-slider", true, false, 991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+        var slider = $($slidername);
+        var settings = {
+            mobileFirst: true,
+            dots: $dots,
+            arrows: $arrows,
+            responsive: [{
+                breakpoint: $breakpoint,
+                settings: "unslick"
+            }]
+        };
+
+        slider.slick(settings);
+
+        $(window).on("resize", function() {
+            if ($(window).width() > $breakpoint) {
+                return;
+            }
+            if (!slider.hasClass("slick-initialized")) {
+                return slider.slick(settings);
+            }
+        });
+    }
